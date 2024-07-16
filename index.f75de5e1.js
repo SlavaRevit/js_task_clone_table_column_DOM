0fd6d4c401b8ddf1fc2bff0f5e1aacd21d7d70ff
Slava Filimonenko <=>
2024-07-16 23:35:41 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const table = document.querySelector("table");
+const columnIndex = 1;
+function copyColumn(tableData, index) {
+    const sections = [
+        tableData.tHead,
+        ...tableData.tBodies,
+        tableData.tFoot
+    ];
+    sections.forEach((section)=>{
+        if (section) for(let i = 0; i < section.rows.length; i++){
+            const row = section.rows[i];
+            const cell = row.cells[index];
+            if (cell) {
+                const newCell = section === table.tHead || section === table.tFoot ? document.createElement("th") : document.createElement("td");
+                newCell.textContent = cell.textContent || cell.innerText;
+                row.insertBefore(newCell, row.cells[row.cells.length - 1]);
+            }
+        }
+    });
+}
+copyColumn(table, columnIndex);
+
+//# sourceMappingURL=index.f75de5e1.js.map
